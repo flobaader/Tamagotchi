9499ff252960a019fec08b3c0642cb9294ea68ce
Unknown <tonychristl1@icloud.com>
2017-06-06 14:35:35 +0200

    Edit Project

---
+++ Tamagotchi.java
@@ -0,0 +1,26 @@
+public class Tamagotchi
+{
+    public String name;  
+    public int age;    
+    public String mood;
+    public String fatigue;
+    public boolean hunger;
+    public int toilet;
+    public int hygiene;
+    public int money;
+    public int mass;
+    public int healthpoints;
+    public int level;
+    public Costume costume;
+    public Figure figure;
+    public Background background;
+    public Achievement achievement;
+    public Sound sound;
+    
+    public Tamagotchi()
+    {
+        
+    }
+
+}
+
